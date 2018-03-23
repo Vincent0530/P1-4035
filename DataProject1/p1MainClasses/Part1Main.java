9ee39543f66e9f818d034d21de86a4c5f1c81dc4
Vincent <Vincent@LAPTOP-306BO3U0>
2018-03-22 23:31:14 -0400

    Part1Main can run from the terminal

--- DataProject1/p1MainClasses/Part1Main.java
+++ DataProject1/p1MainClasses/Part1Main.java
@@ -14,6 +14,8 @@ import setIntersectionFinders.P4IntersectionFinder;
 import setUnionFinder.UnionFinder;
 
 public class Part1Main {
+	private static int parms = 0;
+	
 	public static void main(String[] args) throws FileNotFoundException{
 		
 		DataReader reader = new DataReader();
@@ -22,6 +24,10 @@ public class Part1Main {
 		UnionFinder<Integer> unionFinder = new UnionFinder<Integer>();
 		MySet<Integer>[] set1Array = unionFinder.findUnionSet1(filesInfo); 
 		MySet<Integer>[] set2Array = unionFinder.findUnionSet2(filesInfo); 
+		
+		if (args.length!=0)
+			parms = Integer.parseInt(args[0]); 
+		
 		
 		P1P2IntersectionFinder finder1 = new P1P2IntersectionFinder("P1");
 		P1P2IntersectionFinder finder2 = new P1P2IntersectionFinder("P2");
@@ -33,14 +39,28 @@ public class Part1Main {
 		Set2<Integer> P3intersection = (Set2<Integer>)(finder3.intersectSets(set2Array));
 		Set2<Integer> P4intersection = (Set2<Integer>)(finder4.intersectSets(set2Array));
 		
-		for(int i=0; i<reader.getM(); i++)
-			System.out.println(set1Array[i]);
+		if(args.length == 0){
+			System.out.println(finder1.getName()+P1intersection);
+			System.out.println(finder2.getName()+P2intersection);
+			System.out.println(finder3.getName()+P3intersection);
+			System.out.println(finder4.getName()+P4intersection);
+		}
 		
-		System.out.println(finder1.getName()+P1intersection);
-		System.out.println(finder2.getName()+P2intersection);
-		System.out.println(finder3.getName()+P3intersection);
-		System.out.println(finder4.getName()+P4intersection);
-		System.out.println();	
+		else if(parms == 1){
+			System.out.println(finder1.getName()+P1intersection);
+		}
+		
+		else if(parms == 2){
+			System.out.println(finder2.getName()+P2intersection);
+		}
+		
+		else if(parms == 3){
+			System.out.println(finder2.getName()+P3intersection);
+		}
+		
+		else if(parms == 4){
+			System.out.println(finder4.getName()+P4intersection);
+		}
 	}
 	
 }
